ab958384c719243ad2b6204cc8482ea60e94d26f
ch <mkmsacc@outlook.com>
2020-11-03 21:14:39 +0700

    add StorageException

---
+++ src/main/java/com/example/uploadingfiles/storage/StorageProperties.java
@@ -0,0 +1,12 @@
+package com.example.uploadingfiles.storage;
+
+import java.net.URI;
+
+public class StorageProperties {
+
+	public URI getLocation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
